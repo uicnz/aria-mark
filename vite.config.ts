a4279e65bce74ad20a7e508d8bee309a2839be18
Shane Holloman <shaneholloman@gmail.com>
2026-01-16 21:37:24 +1300

    Add custom domain yoyo.io

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/aria-mark/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
